203f3b6e1426a04ec83532050853f4a4efb694ad
Julien <julienhalde9@gmail.com>
2024-12-30 17:06:15 -0500

    Creation of Flask App

--- stockScraper.py
+++ stockScraper.py
@@ -3,91 +3,40 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-# Define headers to mimic a browser request
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
-}
 
-# 5 tickers
-test_tickers = [
-    'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'BRK.B'
-]
+def scrape_data(tickers):
+    # Define headers to mimic a browser request
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+    }
+    data = []
 
-# Almost all the 500 compagnies in the S&P500
-tickers = [
-    'MMM', 'AOS', 'ABT', 'ABBV', 'ACN', 'ADBE', 'AMD', 'AES', 'AFL', 'A',
-    'APD', 'AKAM', 'ALK', 'ALB', 'ARE', 'ALGN', 'ALLE', 'LNT', 'ALL', 'GOOGL',
-    'GOOG', 'MO', 'AMZN', 'AMCR', 'AEE', 'AAL', 'AEP', 'AXP', 'AIG', 'AMT',
-    'AWK', 'AMP', 'ABC', 'AME', 'AMGN', 'APH', 'ADI', 'ANSS', 'AON', 'APA',
-    'AAPL', 'AMAT', 'APTV', 'ANET', 'AJG', 'AIZ', 'T', 'ATO', 'ADSK', 'AZO',
-    'AVB', 'AVY', 'BKR', 'BALL', 'BAC', 'BBWI', 'BAX', 'BDX', 'WRB', 'BRK.B',
-    'BBY', 'BIO', 'TECH', 'BIIB', 'BLK', 'BK', 'BA', 'BKNG', 'BWA', 'BXP',
-    'BSX', 'BMY', 'AVGO', 'BR', 'BRO', 'BF.B', 'CHRW', 'CDNS', 'CZR', 'CPB',
-    'COF', 'CAH', 'KMX', 'CCL', 'CARR', 'CTLT', 'CAT', 'CBOE', 'CBRE', 'CDW',
-    'CE', 'CNC', 'CNP', 'CDAY', 'CERN', 'CF', 'CRL', 'SCHW', 'CHTR', 'CVX',
-    'CMG', 'CB', 'CHD', 'CI', 'CINF', 'CTAS', 'CSCO', 'C', 'CFG', 'CTXS',
-    'CLX', 'CME', 'CMS', 'KO', 'CTSH', 'CL', 'CMCSA', 'CMA', 'CAG', 'COP',
-    'ED', 'STZ', 'CEG', 'COO', 'CPRT', 'GLW', 'CTVA', 'COST', 'CTRA', 'CCI',
-    'CSX', 'CMI', 'CVS', 'DHI', 'DHR', 'DRI', 'DVA', 'DE', 'DAL', 'XRAY',
-    'DVN', 'DXCM', 'FANG', 'DLR', 'DFS', 'DISCA', 'DISCK', 'DISH', 'DIS',
-    'DG', 'DLTR', 'D', 'DPZ', 'DOV', 'DOW', 'DTE', 'DUK', 'DRE', 'DD', 'DXC',
-    'EMN', 'ETN', 'EBAY', 'ECL', 'EIX', 'EW', 'EA', 'EMR', 'ENPH', 'ETR',
-    'EOG', 'EPAM', 'EFX', 'EQIX', 'EQR', 'ESS', 'EL', 'ETSY', 'RE', 'EVRG',
-    'ES', 'EXC', 'EXPE', 'EXPD', 'EXR', 'XOM', 'FFIV', 'FDS', 'FAST', 'FRT',
-    'FDX', 'FITB', 'FRC', 'FE', 'FIS', 'FISV', 'FLT', 'FMC', 'F', 'FTNT',
-    'FTV', 'FBHS', 'FOXA', 'FOX', 'BEN', 'FCX', 'GRMN', 'IT', 'GE', 'GNRC',
-    'GD', 'GIS', 'GM', 'GPC', 'GILD', 'GL', 'GPN', 'GS', 'HAL', 'HBI', 'HIG',
-    'HAS', 'HCA', 'PEAK', 'HSIC', 'HSY', 'HES', 'HPE', 'HLT', 'HOLX', 'HD',
-    'HON', 'HRL', 'HST', 'HWM', 'HPQ', 'HUM', 'HBAN', 'HII', 'IBM', 'IEX',
-    'IDXX', 'INFO', 'ITW', 'ILMN', 'INCY', 'IR', 'INTC', 'ICE', 'IP', 'IPG',
-    'IFF', 'INTU', 'ISRG', 'IVZ', 'IPGP', 'IQV', 'IRM', 'JBHT', 'JKHY', 'J',
-    'SJM', 'JNJ', 'JCI', 'JPM', 'JNPR', 'K', 'KEY', 'KEYS', 'KMB', 'KIM',
-    'KMI', 'KLAC', 'KHC', 'KR', 'LHX', 'LH', 'LRCX', 'LW', 'LVS', 'LEG',
-    'LDOS', 'LEN', 'LLY', 'LNC', 'LIN', 'LYV', 'LKQ', 'LMT', 'L', 'LOW',
-    'LUMN', 'LYB', 'MTB', 'MRO', 'MPC', 'MKTX', 'MAR', 'MMC', 'MLM', 'MAS',
-    'MA', 'MTCH', 'MKC', 'MCD', 'MCK', 'MDT', 'MRK', 'META', 'MET', 'MTD',
-    'MGM', 'MCHP', 'MU', 'MSFT', 'MAA', 'MRNA', 'MHK', 'MOH', 'TAP', 'MDLZ',
-    'MPWR', 'MNST', 'MCO', 'MS', 'MOS', 'MSI', 'MSCI', 'NDAQ', 'NTAP', 'NFLX',
-    'NWL', 'NEM', 'NWSA', 'NWS', 'NEE', 'NLSN', 'NKE', 'NI', 'NDSN', 'NSC',
-    'NTRS', 'NOC', 'NLOK', 'NCLH', 'NOV', 'NRG', 'NUE', 'NVDA', 'NVR', 'NXPI',
-    'ORLY', 'OXY', 'ODFL', 'OMC', 'OKE', 'ORCL', 'OGN', 'OTIS', 'PCAR', 'PKG',
-    'PARA', 'PH', 'PAYX', 'PAYC', 'PYPL', 'PNR', 'PEP', 'PKI', 'PFE', 'PCG',
-    'PM', 'PSX', 'PNW', 'PXD', 'PNC', 'POOL', 'PPG', 'PPL', 'PFG', 'PG',
-    'PGR', 'PLD', 'PRU', 'PTC'
-]
-
-# Url to loop over
-urls = [f'https://finance.yahoo.com/quote/{ticker.replace("-", ".")}/' for ticker in test_tickers] #! to replace to tickers
-
-infos = []
-
-for url in urls:
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status() 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # Stock Ticker
-        ticker = soup.find("h1", {"class": "yf-xxbei9"}).text.strip()
-        # Stock price
-        price = soup.find("fin-streamer", {"data-field": "regularMarketPrice"}).text.strip()
-        # Price change
-        change = soup.find("fin-streamer", {"data-field": "regularMarketChangePercent"}).text.strip()
-        # Volume
-        volume = soup.find('fin-streamer', {'data-field': 'regularMarketVolume'}).text.strip()
-        
-        current_infos = [ticker, price, change, volume]
-        infos.append(current_infos)
-        
-        print(f"Ticker: {ticker}")
-        print(f"Price: {price}")
-        print(f"Change: {change}")
-        print(f"Volume: {volume}")
-        print("-" * 50)
-        
-    except Exception as e:
-        print(f"Error fetching data for URL {url}: {e}")
-        
-    # try not to get banned by putting a 5 sec between requests
-    time.sleep(5)
-        
+    for ticker in tickers:
+        url = f'https://finance.yahoo.com/quote/{ticker}/'
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status() 
+            soup = BeautifulSoup(response.text, 'html.parser')
+            
+            # Stock Ticker
+            ticker_name = soup.find("h1", {"class": "yf-xxbei9"}).text.strip()
+            # Stock price
+            price = soup.find("fin-streamer", {"data-field": "regularMarketPrice"}).text.strip()
+            # Price change
+            change = soup.find("fin-streamer", {"data-field": "regularMarketChangePercent"}).text.strip()
+            # Volume
+            volume = soup.find('fin-streamer', {'data-field': 'regularMarketVolume'}).text.strip()
+            
+            data.append({
+                'ticker': ticker_name,
+                'price': price,
+                'change': change,
+                'volume': volume
+            })
+            
+        except Exception as e:
+            print(f"Error fetching data for URL {url}: {e}")            
+        # try not to get banned by putting a 5 sec between requests
+        # time.sleep(5)
+    return data
+            
